95cbe889d1fec81daa77d85089ea7dd24c1c557f
Damir Lapeiko <lapeiko02@maik.ru>
2023-02-22 18:53:38 +0300

    Changing features

--- src/by/teachmeskills/lapeiko/homework13/IntList.java
+++ src/by/teachmeskills/lapeiko/homework13/IntList.java
@@ -6,6 +6,7 @@ public class IntList {
     private int[] list;
 
     public IntList() {
+        this.list = new int[]{};
     }
 
     public IntList(int[] list) {
@@ -17,7 +18,7 @@ public class IntList {
     }
 
     public int get(int index) {
-        if (list.length == 0 || index < 0 || index >= list.length) {
+        if (index < 0 || index >= list.length) {
             throw new IllegalArgumentException("Error. Verify your code.");
         }
         return list[index];
@@ -33,24 +34,17 @@ public class IntList {
     }
 
     public int size() {
-        if (list.length != 0) {
-            return list.length;
-        } else {
-            return 0;
-        }
+        return list.length;
     }
 
     public void add(int element) {
         int[] newList;
         if (list.length != 0) {
             newList = new int[list.length + 1];
-        } else {
-            newList = new int[1];
+            System.arraycopy(list, 0, newList, 0, list.length);
+            newList[list.length] = element;
+            list = newList;
         }
-        System.arraycopy(list, 0, newList, 0, list.length);
-        newList[list.length] = element;
-        list = newList;
-        System.out.println(Arrays.toString(newList));
     }
 
     public int remove(int index) {
@@ -64,6 +58,7 @@ public class IntList {
             if (index == i) {
                 element = newList[i];
             }
+            list = newList;
         }
         return element;
     }
@@ -75,7 +70,6 @@ public class IntList {
             throw new IllegalArgumentException("Error. Incorrect first or last index.");
         }
         int[] subList = Arrays.copyOfRange(list, startIndexInclusive, endIndexExclusive);
-        System.out.println(Arrays.toString(subList));
         return new IntList(subList);
     }
 
@@ -84,16 +78,14 @@ public class IntList {
                 startIndexInclusive > list.length) {
             throw new IllegalArgumentException("Error. Inadmissible value of index.");
         }
-        int[] subList = Arrays.copyOfRange(list, startIndexInclusive, list.length);
-        System.out.println(Arrays.toString(subList));
-        return new IntList(subList);
+        return subList(startIndexInclusive, list.length);
     }
 
     public int lastIndexOf(int element) {
         if (list.length == 0) {
             throw new IllegalArgumentException("Nothing to search.");
         }
-        for (int i = 0; i < list.length; i++) {
+        for (int i = list.length - 1; i >= 0; i--) {
             if (list[i] == element) {
                 return i;
             }
